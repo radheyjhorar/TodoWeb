937b260be6ff2adac538fa2d1acb7ff108c1d6cb
radheyjhorar <shyamjhorar700@gmail.com>
2025-07-18 23:58:00 +0530

    new MogoURI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,37 +2,30 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Connected');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(error)
+    console.error(`MongoDB connection error: ${error.message}`);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://radheyjhorar21:<radheyjhorar3608>@clusterradheyjhorar.2szuefx.mongodb.net/?retryWrites=true&w=majority&appName=ClusterRadheyJhorar";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+
+
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log('MongoDB Connected!');
+//   } catch (error) {
+//     console.log(error)
+//     process.exit(1);
+//   }
+// }
+
+// module.exports = connectDB;
--- backend/main.js
+++ backend/main.js
@@ -8,17 +8,21 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const { protect } = require('./middleware/authMiddleware')
 
 dotenv.config();
-connectDB();
 
 const app = express();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/todos', protect, todoRoutes);
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.use(errorHandler);
+// app.use('/api/users', userRoutes);
+// app.use('/api/todos', protect, todoRoutes);
+
+// app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
