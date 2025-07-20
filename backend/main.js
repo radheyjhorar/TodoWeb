741c5d2b65cc31dfec9947dcd88ebab58e95efc7
radheyjhorar <shyamjhorar700@gmail.com>
2025-07-20 09:11:15 +0530

    new route for test api is running or not

--- backend/main.js
+++ backend/main.js
@@ -17,9 +17,9 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("API is running...");
-// });
+app.get("/apitest", (req, res) => {
+  res.send("API is running...");
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/todos', protect, todoRoutes);
